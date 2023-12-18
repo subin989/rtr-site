4e9034b02d96918aa4b158ceef14767926cd3d61
subin989 <subin@konnectcraft.com>
2023-12-18 21:20:47 +0545

    Add form validation and functionality o send the mail

--- components/Contact/index.tsx
+++ components/Contact/index.tsx
@@ -1,4 +1,54 @@
-const Contact = () => {
+"use client";
+
+import { FC, useState } from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { notification, Spin } from "antd";
+
+export type FormData = {
+  name: string;
+  email: string;
+  message: string;
+};
+
+const Contact: FC = () => {
+  const { register, handleSubmit ,reset} = useForm<FormData>();
+
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const onSubmit: SubmitHandler<FormData> = (data) => {
+    setLoading(true);
+    sendEmail(data);
+  };
+  function sendEmail(data: FormData) {
+    const apiEndpoint = "/api/email";
+    fetch(apiEndpoint, {
+      method: "POST",
+      body: JSON.stringify(data),
+    })
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return res.json();
+      })
+      .then((response) => {
+        notification.success({
+          message: "Success",
+          description: response.message,
+        });
+        setLoading(false);
+        reset();
+
+      })
+      .catch((err) => {
+        notification.error({
+          message: "Error",
+          description: err.message || "Something went wrong!",
+        });
+        setLoading(false);
+      });
+  }
+
   return (
     <section id="contact" className="overflow-hidden py-16 md:py-20 lg:py-28">
       <div className="container">
@@ -7,7 +57,7 @@ const Contact = () => {
             <div
               className="wow fadeInUp mb-12 rounded-md bg-primary/[3%] py-11 px-8 dark:bg-dark sm:p-[55px] lg:mb-5 lg:px-8 xl:p-[55px]"
               data-wow-delay=".15s
-              "
+            "
             >
               <h2 className="mb-3 text-2xl font-bold text-black dark:text-white sm:text-3xl lg:text-2xl xl:text-3xl">
                 Need Help? Open a Ticket
@@ -15,7 +65,7 @@ const Contact = () => {
               <p className="mb-12 text-base font-medium text-body-color">
                 Our support team will get back to you ASAP via email.
               </p>
-              <form>
+              <form onSubmit={handleSubmit(onSubmit)}>
                 <div className="-mx-4 flex flex-wrap">
                   <div className="w-full px-4 md:w-1/2">
                     <div className="mb-8">
@@ -29,6 +79,7 @@ const Contact = () => {
                         type="text"
                         placeholder="Enter your name"
                         className="w-full rounded-md border border-transparent py-3 px-6 text-base text-body-color placeholder-body-color shadow-one outline-none focus:border-primary focus-visible:shadow-none dark:bg-[#242B51] dark:shadow-signUp"
+                        {...register("name", { required: true })}
                       />
                     </div>
                   </div>
@@ -44,6 +95,7 @@ const Contact = () => {
                         type="email"
                         placeholder="Enter your email"
                         className="w-full rounded-md border border-transparent py-3 px-6 text-base text-body-color placeholder-body-color shadow-one outline-none focus:border-primary focus-visible:shadow-none dark:bg-[#242B51] dark:shadow-signUp"
+                        {...register("email", { required: true })}
                       />
                     </div>
                   </div>
@@ -60,12 +112,20 @@ const Contact = () => {
                         rows={5}
                         placeholder="Enter your Message"
                         className="w-full resize-none rounded-md border border-transparent py-3 px-6 text-base text-body-color placeholder-body-color shadow-one outline-none focus:border-primary focus-visible:shadow-none dark:bg-[#242B51] dark:shadow-signUp"
+                        {...register("message", { required: true })}
                       ></textarea>
                     </div>
                   </div>
                   <div className="w-full px-4">
-                    <button className="rounded-md bg-primary py-4 px-9 text-base font-medium text-white transition duration-300 ease-in-out hover:bg-opacity-80 hover:shadow-signUp">
-                      Submit Ticket
+                    <button
+                      className="rounded-md bg-primary py-4 px-9 text-base font-medium text-white transition duration-300 ease-in-out hover:bg-opacity-80 hover:shadow-signUp"
+                      type="submit"
+                    >
+                      {loading ? (
+                        <Spin /> // Display Ant Design Spin while loading
+                      ) : (
+                        "Submit Ticket"
+                      )}
                     </button>
                   </div>
                 </div>
