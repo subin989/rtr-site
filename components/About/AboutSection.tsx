f3e9ab0d150fd225cdac16b2affccd94794c40ca
subin989 <subin@konnectcraft.com>
2023-12-20 22:21:07 +0545

    Rename the component and delete some component

--- components/About/AboutSection.tsx
+++ components/About/AboutSection.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 
-const AboutSectionTwo = () => {
+const AboutSection = () => {
   return (
     <section className="py-16 md:py-20 lg:py-28">
       <div className="container">
@@ -10,11 +10,7 @@ const AboutSectionTwo = () => {
               className="wow fadeInUp relative mx-auto mb-12 aspect-[25/24] max-w-[500px] text-center lg:m-0"
               data-wow-delay=".15s"
             >
-              <Image
-                src="/images/logo/logo.png"
-                alt="about image"
-                fill
-              />
+              <Image src="/images/logo/logo.png" alt="about image" fill />
             </div>
           </div>
           <div className="w-full px-4 lg:w-1/2">
@@ -23,27 +19,32 @@ const AboutSectionTwo = () => {
                 <h3 className="mb-4 text-xl font-bold text-black dark:text-white sm:text-2xl lg:text-xl xl:text-2xl">
                   Mission
                 </h3>
-                <p className="text-base font-medium leading-relaxed text-body-color sm:text-lg sm:leading-relaxed">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                <p className="text-base font-medium leading-relaxed text-body-color sm:text-lg sm:leading-relaxed text-justify">
+                  To cultivate a welcoming community, foster meaningful
+                  connections, and create enriching experiences that promote
+                  camaraderie, personal growth, and shared interests among its
+                  members.
                 </p>
               </div>
               <div className="mb-9">
                 <h3 className="mb-4 text-xl font-bold text-black dark:text-white sm:text-2xl lg:text-xl xl:text-2xl">
                   Vision
                 </h3>
-                <p className="text-base font-medium leading-relaxed text-body-color sm:text-lg sm:leading-relaxed">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                  do eiusmod tempor incididunt.
+                <p className="text-base font-medium leading-relaxed text-body-color sm:text-lg sm:leading-relaxed text-justify">
+                  To create positive and long lasting impact in community by
+                  fostering leadership, promoting sustainable development, and
+                  empowering individuals to reach their full potential with the
+                  value of fellowship.
                 </p>
               </div>
               <div className="mb-1">
                 <h3 className="mb-4 text-xl font-bold text-black dark:text-white sm:text-2xl lg:text-xl xl:text-2xl">
                   Goal
                 </h3>
-                <p className="text-base font-medium leading-relaxed text-body-color sm:text-lg sm:leading-relaxed">
-                  Lorem ipsum dolor sit amet, sed do eiusmod tempor incididunt
-                  consectetur adipiscing elit setim.
+                <p className="text-base font-medium leading-relaxed text-body-color sm:text-lg sm:leading-relaxed text-justify">
+                  Empowering communities through impactful projects, fostering
+                  vibrant engagement, and promoting leadership for a better
+                  tomorrow.
                 </p>
               </div>
             </div>
@@ -54,4 +55,4 @@ const AboutSectionTwo = () => {
   );
 };
 
-export default AboutSectionTwo;
+export default AboutSection;
