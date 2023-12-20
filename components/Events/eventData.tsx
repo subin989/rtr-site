1087a682a6da55f9bb66bad7cedde92a8be2fd12
subin989 <subin@konnectcraft.com>
2023-12-20 22:22:53 +0545

    Add new event details

--- components/Events/eventData.tsx
+++ components/Events/eventData.tsx
@@ -3,10 +3,10 @@ import { Blog } from "@/types/blog";
 const eventData: Blog[] = [
   {
     id: 1,
-    title: "Club Installation",
+    title: "18th Chater's Day",
     paragraph:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sit amet dictum neque, laoreet dolor.",
-    image: "/images/events/installation.jpg",
+      "During the Charter's Day celebrations in Baneshwor, all Rotaractors come together for a special gathering. The event includes a symbolic cake to honor the club's charter anniversary. It is accompanied by delightful food and lively music, creating a joyful and festive atmosphere for everyone to enjoy.",
+    image: "/images/events/chaters.jpg",
     author: {
       name: "Rtr. Subin Satyal",
       image: "/images/events/author-02.png",
@@ -14,36 +14,135 @@ const eventData: Blog[] = [
       designation: "Club Admin",
     },
     tags: ["Club Admin"],
-    publishDate: "2023-01-23",
+    publishDate: "2023-07-06",
   },
   {
     id: 2,
-    title: "CV Writing",
+    title: "1st Club Assembly",
     paragraph:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sit amet dictum neque, laoreet dolor.",
-    image: "/images/events/cv.jpg",
+    image: "/images/events/assembly.jpeg",
     author: {
-      name: "Rtr. Simran Poudel",
+      name: "Rtr. Subin Satyal",
       image: "/images/events/author-01.png",
 
-      designation: "Professional Development Chair",
+      designation: "Club Admin",
     },
-    tags: ["Professional"],
-    publishDate: "2023-03-15",
+    tags: ["Club Admin"],
+    publishDate: "2023-07-08",
   },
   {
     id: 3,
-    title: "Tree Plantation",
+    title:
+      "Joint Installation of Rotary Club of Budanilkantha",
     paragraph:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sit amet dictum neque, laoreet dolor.",
-    image: "/images/events/tree.jpg",
+      "The joint installation ceremony of the Rotary Club of Budanilkantha and the Rotaract Club of Budanilkantha took place at Fresh Farm in Budanilkantha on August 5th, officially commencing at 5:50 PM.",
+    image: "/images/events/installation-ceremony.jpg",
     author: {
-      name: "Rtr. Abiral Shretha",
+      name: "Rtr. Subin Satyal",
+      image: "/images/events/author-03.png",
+      designation: "Club Admin",
+    },
+    tags: ["Club Admin"],
+    publishDate: "2023-08-05",
+  },
+  {
+    id: 4,
+    title: "Friendship Day Celebration",
+    paragraph:
+      "The Rotaract Club of Budhanilkantha organized a special event on the occasion of World Friendship Day to foster camaraderie and promote fellowship among its members. The event took place at Chiya Ghar, Sinamangal on July 29th, Saturday, from 8:30 AM onwards. The primary objective of the gathering was to celebrate the spirit of friendship, discuss the club's plans and strategies, and strengthen the bonds within the club.",
+    image: "/images/events/friendship-day-celebration.jpg",
+    author: {
+      name: "Rtr.Shishir Aryal",
+      image: "/images/events/author-03.png",
+      designation: "Secretary",
+    },
+    tags: ["Club Service"],
+    publishDate: "2023-07-29",
+  },
+  {
+    id: 5,
+    title: "CV Writing",
+    paragraph:
+      "Our experienced CV writing trainer provide us with step-by-step guidance and valuable insights to create a standout CV.",
+    image: "/images/events/cv.jpg",
+    author: {
+      name: "Rtr.Simran Poudel",
+      image: "/images/events/author-03.png",
+      designation: "Professional Development Chair",
+    },
+    tags: ["Professional"],
+    publishDate: "2023-08-30",
+  },
+  {
+    id: 6,
+    title: "GoodWill Visit",
+    paragraph:
+      "Rotaract Club of Budhanilkantha went on a Goodwill visit with the Rotaract Club of Kathmandu North. We participated in their 276th General Meeting. 17th members from the Rotaract Club of Budhanilkantha participated in the Goodwill visit.",
+    image: "/images/events/cv.jpg",
+    author: {
+      name: "Rtr. Udit Mahato",
+      image: "/images/events/author-03.png",
+      designation: "President",
+    },
+    tags: ["Club Admin"],
+    publishDate: "2023-09-09",
+  },
+  {
+    id: 7,
+    title: "Teej Event (Dar for Charity)",
+    paragraph:
+      "We officially commenced our Teej Event (Dar for charity) at 2 PM at Sunway College, organised by Club Administration Chair Rtr. Subin Satyal along with a few organising members.",
+    image: "/images/events/teej-event.jpg",
+    author: {
+      name: "Rtr. Subin Satyal",
+      image: "/images/events/author-03.png",
+      designation: "Club Admin",
+    },
+    tags: ["Club Admin"],
+    publishDate: "2023-09-16",
+  },
+  {
+    id: 8,
+    title: "Candles of Hope",
+    paragraph:
+      "On 10th of October, Rotaract Club of Budhanilkantha and few guests from Sunway College gathered in Maitidevi Temple on the occasion of World Mental Health Day to pay respect to all the soul who lost their lives due to mental illness as well to those innocent souls who lost their lives in the Israel-Palestine War. ",
+    image: "/images/events/candel.jpg",
+    author: {
+      name: "Rtr. Abiral Shrestha",
       image: "/images/events/author-03.png",
       designation: "Club Service Chair",
     },
     tags: ["Club Service"],
-    publishDate: "2023-04-12",
+    publishDate: "2023-10-10",
+  },
+  {
+    id: 9,
+    title: "Plant Adoption Campaign",
+    paragraph:
+      "On 10th of September, Rotaract Club of Budhanilkantha started an online Tree Adoption Campaign within the club. Over a period of 28 days, 10 Rotaractors adopted 16 plants in their homes and contributed towards the campaign. ",
+    image: "/images/events/tree-plantation.jpg",
+    author: {
+      name: "Rtr. Abiral Shrestha",
+      image: "/images/events/author-03.png",
+      designation: "Club Service Chair",
+    },
+    tags: ["Club Service"],
+    publishDate: "2023-09-10",
+  },
+  {
+    id: 10,
+    title: "Letter Head Exchange with SD College, Chandigarh, India RID: 3080",
+    paragraph:
+      "The event was a Global Letterhead Exchange, initiated by Rotaract Club of SD College, Chandigarh, India RID: 3080. As the medium was virtual, the event ran short yet productive. There were 5 other clubs present as well. We had a round of greetings exchange and a brief introduction about our clubs followed by the letterhead exchange and then an end note by all.  ",
+    image: "/images/events/installation.jpg",
+    author: {
+      name: "	Rtr. Sushna Gurung",
+      image: "/images/events/author-03.png",
+      designation: "Internation Service Chair",
+    },
+    tags: ["Internation Service"],
+    publishDate: "2023-11-10",
   },
 ];
 export default eventData;
