dc3c2f86c7eac1b6e4c7435ed5fcb538345197eb
subin989 <subin@konnectcraft.com>
2023-12-21 22:03:12 +0545

    new route to show the event details

---
+++ app/event-details/page.tsx
@@ -0,0 +1,11 @@
+import EventDetailsPage from "@/components/EventDetails/eventDetails";
+
+const EventDetails = () => {
+  return (
+    <>
+      <EventDetailsPage />
+    </>
+  );
+};
+
+export default EventDetails;
